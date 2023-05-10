c77ab65d30e5735ca204daa6aa512db53297bc57
melloc1801 <dmitry.tomahawk@yandex.ru>
2023-05-10 18:38:00 +0500

    add unique index to login

--- VK.Infrastructure/Persistence/Migrations/20230510133449_UserLoginUnique.Designer.cs
+++ VK.Infrastructure/Persistence/Migrations/20230510133449_UserLoginUnique.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VK.Infrastructure.Persistence.Context;
@@ -11,9 +12,11 @@ using VK.Infrastructure.Persistence.Context;
 namespace VK.Infrastructure.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230510133449_UserLoginUnique")]
+    partial class UserLoginUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
